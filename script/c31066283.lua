9aabf41849a48aea4464b99a46800ee5525fa864
nvasfer <nvasfer@gmail.com>
2022-11-05 06:19:49 -0300

    update

---
+++ script/c31066283.lua
@@ -0,0 +1,7 @@
+Duel.LoadScript("cube-attrib-rotation.lua")
+
+--スカルライダーの復活
+local s,id=GetID()
+function s.initial_effect(c)
+	Ritual.AddProcGreaterCode(c,6,nil,99721536)
+end
---
+++ script/c43694075.lua
@@ -0,0 +1,7 @@
+Duel.LoadScript("cube-attrib-rotation.lua")
+
+--ローの祈り
+local s,id=GetID()
+function s.initial_effect(c)
+	Ritual.AddProcGreaterCode(c,7,nil,3627449)
+end
