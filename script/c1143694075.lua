bf904df39472f65be0125c6113c5c6106214503c
nvasfer <nvasfer@gmail.com>
2022-11-06 06:06:21 -0300

    "update"

--- script/c1143694075.lua
+++
@@ -1,7 +0,0 @@
-Duel.LoadScript("cube-attrib-rotation.lua")
-
---ローの祈り
-local s,id=GetID()
-function s.initial_effect(c)
-	Ritual.AddProcGreaterCode(c,7,nil,3627449)
-end
---
+++ script/c1146696593.lua
@@ -0,0 +1,9 @@
+Duel.LoadScript("cube-attrib-rotation.lua")
+
+--紅陽鳥
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,75582395,10202894)
+end
