aa6a99f03827f17beacc9d3b421ec4c06f979519
nvasfer <nvasfer@gmail.com>
2022-11-06 08:08:57 -0300

    "update"

--- script/c1155761792.lua
+++
@@ -1,7 +0,0 @@
-Duel.LoadScript("cube-attrib-rotation.lua")
-
---カオスの儀式
-local s,id=GetID()
-function s.initial_effect(c)
-	Ritual.AddProcGreaterCode(c,8,nil,5405694)
-end
--- script/c1199721536.lua
+++
@@ -1,8 +0,0 @@
-Duel.LoadScript("cube-attrib-rotation.lua")
-
---スカルライダー
-local s,id=GetID()
-function s.initial_effect(c)
-	c:EnableReviveLimit()
-end
-s.listed_names={31066283}
