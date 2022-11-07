70470bba7e416eae6769d2b36ec1acc0c90ece20
nvasfer <nvasfer@gmail.com>
2022-11-07 03:19:52 -0300

    "update"

--- script/c1160369732.lua
+++
@@ -1,7 +0,0 @@
-Duel.LoadScript("cube-attrib-rotation.lua")
-
---大邪神の儀式
-local s,id=GetID()
-function s.initial_effect(c)
-	Ritual.AddProcGreaterCode(c,8,nil,62420419)
-end
--- script/c1180516007.lua
+++
@@ -1,9 +0,0 @@
-Duel.LoadScript("cube-attrib-rotation.lua")
-
---レア・フィッシュ
-local s,id=GetID()
-function s.initial_effect(c)
-	--fusion material
-	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,1641882,75376965)
-end
