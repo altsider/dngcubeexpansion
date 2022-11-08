7310f0f32ba39c4cd4df726944fcab370557ae3a
nvasfer <nvasfer@gmail.com>
2022-11-08 05:10:45 -0300

    "update"

---
+++ script/c1108198712.lua
@@ -0,0 +1,8 @@
+Duel.LoadScript("cube-attrib-rotation.lua")
+
+--エンド・オブ・ザ・ワールド
+--End of the World
+local s,id=GetID()
+function s.initial_effect(c)
+	Ritual.AddProcEqualCode(c,nil,nil,72426662,46427957)
+end
--- script/c1133951077.lua
+++
@@ -1,8 +0,0 @@
-Duel.LoadScript("cube-attrib-rotation.lua")
-
---スーパー・ウォー・ライオン
-local s,id=GetID()
-function s.initial_effect(c)
-	c:EnableReviveLimit()
-end
-s.listed_names={54539105}
