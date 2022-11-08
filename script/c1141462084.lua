11e28183c5b007b7f5ef4c4897ab0943b036d327
nvasfer <nvasfer@gmail.com>
2022-11-08 05:15:35 -0300

    "update"

--- script/c1141462084.lua
+++
@@ -1,9 +0,0 @@
-Duel.LoadScript("cube-attrib-rotation.lua")
-
---千年竜
-local s,id=GetID()
-function s.initial_effect(c)
-	--fusion material
-	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,71625222,88819587)
-end
---
+++ script/c1141544074.lua
@@ -0,0 +1,9 @@
+Duel.LoadScript("cube-attrib-rotation.lua")
+
+--カオス・ウィザード
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,15025844,22026707)
+end
