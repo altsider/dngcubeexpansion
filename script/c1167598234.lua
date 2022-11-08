6673e160a0e8c0b4e7a5b3c58d999a58835c83b6
nvasfer <nvasfer@gmail.com>
2022-11-08 05:45:42 -0300

    "update"

---
+++ script/c1167598234.lua
@@ -0,0 +1,10 @@
+Duel.LoadScript("cube-attrib-rotation.lua")
+
+--電影の騎士ガイアセイバー
+--Gaiasaber, the Video Knight
+local s,id=GetID()
+function s.initial_effect(c)
+	--link summon
+	Link.AddProcedure(c,nil,2)
+	c:EnableReviveLimit()
+end
