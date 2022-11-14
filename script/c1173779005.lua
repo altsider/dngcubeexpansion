0bd24f7d1f433c81f56304d154b57008573ef76f
nvasfer <nvasfer@gmail.com>
2022-11-14 10:32:26 -0300

    FIX DRAGOONS OF DRACONIA SCRIPT AGAIN HAAAAAAAAA

--- script/c1173779005.lua
+++ script/c1173779005.lua
@@ -5,18 +5,18 @@ local script, id = GetID()
 
 function script.initial_effect(card)
 	--pendulum summon
-	Pendulum.AddProcedure(c)
+	Pendulum.AddProcedure(card)
 	--to hand
-	local e2=Effect.CreateEffect(c)
+	local e2=Effect.CreateEffect(card)
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_BATTLED)
 	e2:SetRange(LOCATION_PZONE)
 	e2:SetCountLimit(1)
-	e2:SetCondition(s.thcon)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
-	c:RegisterEffect(e2)
+	e2:SetCondition(script.thcon)
+	e2:SetTarget(script.thtg)
+	e2:SetOperation(script.thop)
+	card:RegisterEffect(e2)
 	
 	-- Doubles/halves ATK/DEF based on Attributes
 
@@ -73,7 +73,8 @@ function script.limit_attribute(effect, card)
 
 	return card and card:IsAttribute(opposite)
 end
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+
+function script.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
 	if not d then return false end
@@ -81,17 +82,17 @@ function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return a:IsType(TYPE_NORMAL)
 		and not a:IsStatus(STATUS_BATTLE_DESTROYED) and d:IsStatus(STATUS_BATTLE_DESTROYED)
 end
-function s.filter(c)
+function script.filter(c)
 	return c:IsType(TYPE_NORMAL) and c:IsLevelAbove(4) and c:IsAbleToHand()
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+function script.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(script.filter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
+function script.thop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,script.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
