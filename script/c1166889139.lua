8b69fd39b5739b017563e2bbcfdcb2a545a11233
nvasfer <nvasfer@gmail.com>
2022-11-14 14:51:52 -0300

    fix Gaia

--- script/c1166889139.lua
+++ script/c1166889139.lua
@@ -5,6 +5,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,6368038,28279543)
+	Fusion.AddProcMix(c,true,true,1106368038,1128279543)
 end
 s.material_setcode=0xbd
